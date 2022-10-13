409e12f478ff449cb6a12327973bae9835015004
RaeHoon Baek <bmp117@naver.com>
2022-10-13 21:11:02 +0900

    AppStore 예제 - 앱스토어 앱 상세 화면 구현하기

--- AppStore/Scenes/Today/TodayViewController.swift
+++ AppStore/Scenes/Today/TodayViewController.swift
@@ -49,6 +49,11 @@ extension TodayViewController: UICollectionViewDelegateFlowLayout {
         
         return UIEdgeInsets(top: value, left: value, bottom: value, right: value)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = AppDetailViewController()
+        present(vc, animated: true, completion: nil)
+    }
 }
 
 extension TodayViewController: UICollectionViewDataSource {
